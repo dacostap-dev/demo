c13ddeb14400044d7b492c4b92362b52653eff2c
Daniel Acosta <d4ni3L_15@hotmail.com>
2021-03-19 18:46:56 -0500

    add form section

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@
         <v-tabs align-with-title>
           <v-tab to="/gallery">Galería de Imagenes</v-tab>
 
-          <v-tab to="/form">Sección 2</v-tab>
+          <v-tab to="/form">Formulario</v-tab>
           <v-tab to="/sectionThree">Sección 3</v-tab>
           <v-tab to="/sectionFour">Sección 4</v-tab>
         </v-tabs>
--- src/components/Sections/Form.vue
+++ src/components/Sections/Form.vue
@@ -1,8 +1,171 @@
 <template>
-  <div>
-    <h1>Soy Seccion 2</h1>
-  </div>
+  <v-container fluid>
+    <validation-observer ref="observer" v-slot="{ invalid }">
+      <form @submit.prevent="submit">
+        <validation-provider
+          v-slot="{ errors }"
+          name="El nombre"
+          rules="required|max:15"
+        >
+          <v-text-field
+            v-model="name"
+            :counter="10"
+            :error-messages="errors"
+            label="Nombre"
+            required
+          ></v-text-field>
+        </validation-provider>
+        <validation-provider
+          v-slot="{ errors }"
+          name="El teléfono"
+          :rules="{
+            required: true,
+            digits: 9,
+          }"
+        >
+          <v-text-field
+            v-model="phoneNumber"
+            :counter="7"
+            :error-messages="errors"
+            label="Teléfono"
+            required
+          ></v-text-field>
+        </validation-provider>
+        <validation-provider
+          v-slot="{ errors }"
+          name="El email"
+          rules="required|email"
+        >
+          <v-text-field
+            v-model="email"
+            :error-messages="errors"
+            label="E-mail"
+            required
+          ></v-text-field>
+        </validation-provider>
+        <validation-provider
+          v-slot="{ errors }"
+          name="La nacionalidad"
+          rules="required"
+        >
+          <v-select
+            v-model="select"
+            :items="items"
+            :error-messages="errors"
+            label="Nacionalidad"
+            data-vv-name="select"
+            required
+          ></v-select>
+        </validation-provider>
+
+        <v-btn class="mr-4 mt-2" type="submit" :disabled="invalid">
+          Enviar
+        </v-btn>
+      </form>
+    </validation-observer>
+
+    <template>
+      <v-dialog v-model="dialog" max-width="290">
+        <v-card>
+          <v-card-title class="font-weight-medium">
+            Información del usuario
+          </v-card-title>
+
+          <v-card-text v-if="loading">
+            Enviando...
+            <v-progress-linear
+              indeterminate
+              color="primary"
+              class="mb-0"
+            ></v-progress-linear>
+          </v-card-text>
+          <v-card-text v-else>
+            <div class="font-weight-bold">Nombre: {{ name }}</div>
+            <div class="font-weight-bold">Email: {{ email }}</div>
+            <div class="font-weight-bold">Teléfono: {{ phoneNumber }}</div>
+            <div class="font-weight-bold">Teléfono: {{ select }}</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+
+            <v-btn color="green darken-1" text @click="acceptDialog">
+              Aceptar
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </template>
+  </v-container>
 </template>
 <script>
-export default {};
+import { required, digits, email, max } from "vee-validate/dist/rules";
+import {
+  extend,
+  ValidationObserver,
+  ValidationProvider,
+  setInteractionMode,
+} from "vee-validate";
+
+setInteractionMode("eager");
+
+extend("digits", {
+  ...digits,
+  message: "{_field_} debe tener {length} dígitos. ({_value_})",
+});
+
+extend("required", {
+  ...required,
+  message: "{_field_} no puede estar vacío",
+});
+
+extend("max", {
+  ...max,
+  message: "{_field_} no debe tener más de {length} caracteres",
+});
+
+extend("email", {
+  ...email,
+  message: "El email debe ser válido",
+});
+
+export default {
+  name: "Form",
+  components: {
+    ValidationProvider,
+    ValidationObserver,
+  },
+  data: () => ({
+    name: "Daniel Acosta",
+    phoneNumber: "",
+    email: "",
+    select: null,
+    items: ["Peruano", "Venezolano", "Chileno", "Colombiano"],
+    dialog: false,
+    loading: false,
+  }),
+
+  methods: {
+    async submit() {
+      const res = await this.$refs.observer.validate();
+      if (res) {
+        this.dialog = true;
+        this.loading = true;
+      }
+      setTimeout(() => (this.loading = false), 1000);
+    },
+    acceptDialog() {
+      this.dialog = false;
+      this.clear();
+    },
+    clear() {
+      this.name = "";
+      this.phoneNumber = "";
+      this.email = "";
+      this.select = null;
+      this.checkbox = null;
+      this.$refs.observer.reset();
+    },
+  },
+};
 </script>
